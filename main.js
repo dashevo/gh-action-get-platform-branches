34f84241ce223427c94da1f66b8fc35a19c3e1c5
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-04-01 20:10:04 +0300

    debug

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ core.setOutput('cache-url', process.env['ACTIONS_CACHE_URL']);
 core.setOutput('cache-token', process.env['ACTIONS_RUNTIME_TOKEN']);
 
 const overrideMajorVersion = core.getInput('override-major-version') || undefined;
+const overrideTestSuiteBranch = core.getInput('override-testsuite-branch') || undefined;
+const overrideDashmateBranch = core.getInput('override-dashmate-branch') || undefined;
 
 const { version } = require(`${process.env['GITHUB_WORKSPACE']}/package.json`);
 
 const branch = getBranchFromVersion(version, overrideMajorVersion)
 
-core.setOutput('platform-branch', branch);
+core.setOutput('testsuite-branch', overrideTestSuiteBranch || branch);
+core.setOutput('dashmate-branch', overrideDashmateBranch || branch);
